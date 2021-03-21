30d6225352437af9ea24eec38991adbe8f4dde3e
Philippe Turcotte <pturcotte@xorcube.com>
2021-03-21 01:09:34 +0100

    Added a new page for browsing songs.

---
+++ src/mobile/VoxIA.Mobile/VoxIA.Mobile/Views/SongsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoxIA.Mobile.Models;
+using VoxIA.Mobile.ViewModels;
+using VoxIA.Mobile.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VoxIA.Mobile.Views
+{
+    public partial class SongsPage : ContentPage
+    {
+        SongsViewModel _viewModel;
+
+        public SongsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new SongsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
