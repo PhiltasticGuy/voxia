297d3c7423ce163c0a67a0d36fb2091808c54fe6
Philippe Turcotte <pturcotte@xorcube.com>
2021-04-04 23:35:38 +0200

    Finished the initial version of the media controls and displays.

--- src/mobile/VoxIA.Mobile/VoxIA.Mobile/ViewModels/CurrentSongViewModel.cs
+++ src/mobile/VoxIA.Mobile/VoxIA.Mobile/ViewModels/CurrentSongViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Input;
 using VoxIA.Mobile.Models;
 using VoxIA.Mobile.Services;
@@ -27,6 +28,11 @@ namespace VoxIA.Mobile.ViewModels
         private string _artistName;
         private string _albumCover;
         private bool _isPlaying;
+
+        private readonly Timer _timer = new Timer(1000);
+        private readonly int _length = 10;
+        private int _position = 0;
+        private float _progress = 0f;
 
         public string SongId
         {
@@ -69,6 +75,32 @@ namespace VoxIA.Mobile.ViewModels
 
         public bool IsPaused => !IsPlaying;
 
+        public string PositionText
+        {
+            get => TimeSpan.FromSeconds(_position).ToString(@"mm\:ss");
+        }
+
+        public int Position
+        {
+            get => _position;
+            set
+            {
+                SetProperty(ref _position, value);
+                OnPropertyChanged(nameof(PositionText));
+            }
+        }
+
+        public float SongProgress
+        {
+            get => _progress;
+            set => SetProperty(ref _progress, value);
+        }
+
+        public string SongLength
+        {
+            get => TimeSpan.FromSeconds(_length).ToString(@"mm\:ss");
+        }
+
         public CurrentSongViewModel()
         {
             Title = "Currently Playing";
@@ -77,6 +109,25 @@ namespace VoxIA.Mobile.ViewModels
             PauseSongCommand = new Command(TogglePlayState);
             PreviousSongCommand = new Command(PlayPreviousSong);
             NextSongCommand = new Command(PlayNextSong);
+
+            _timer.Elapsed += (sender, e) =>
+            {
+                Position += 1;
+
+                if (Position == _length)
+                {
+                    _timer.Stop();
+
+                    TogglePlayState();
+
+                    Position = 0;
+                    SongProgress = 0f;
+                }
+                else
+                {
+                    SongProgress = (float)Position / _length;
+                }
+            };
         }
 
         private void TogglePlayState()
@@ -85,6 +136,15 @@ namespace VoxIA.Mobile.ViewModels
 
             // Don't forget to signal a change in 'IsPaused' too!
             OnPropertyChanged(nameof(IsPaused));
+
+            if (IsPlaying)
+            {
+                _timer.Start();
+            }
+            else
+            {
+                _timer.Stop();
+            }
         }
 
         private void PlayPreviousSong()
