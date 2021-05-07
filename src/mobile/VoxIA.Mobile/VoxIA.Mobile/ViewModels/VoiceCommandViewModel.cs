841b588dfc864ab5818695a40b65deed12abf1b7
Philippe Turcotte <pturcotte@xorcube.com>
2021-05-07 16:33:34 +0200

    Added a new Voice Commande page to mobile app.

---
+++ src/mobile/VoxIA.Mobile/VoxIA.Mobile/ViewModels/VoiceCommandViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoxIA.Mobile.ViewModels
+{
+    class VoiceCommandViewModel : BaseViewModel
+    {
+        public VoiceCommandViewModel()
+        {
+            Title = "Voice Command";
+        }
+    }
+}
